62deb6e9de97f7154fe7b83e0ebb1f1ebdfd1c87
CJ <83363242+orbitalmartian8@users.noreply.github.com>
2025-03-13 18:04:58 +0000

    Set Client name

--- src/routes/_api/oauth.js
+++ src/routes/_api/oauth.js
@@ -7,7 +7,7 @@ const SCOPES = 'read write follow push'
 export function registerApplication (instanceName, redirectUri) {
   const url = `${basename(instanceName)}/api/v1/apps`
   return post(url, {
-    client_name: process.env.UPSTREAM ? 'Enafore' : ENAFORE_IS_BROWSER ? location.hostname : 'Enafore',
+    client_name: 'Orbitalfore',
     redirect_uris: redirectUri,
     scopes: SCOPES,
     website: ENAFORE_IS_BROWSER ? location.origin : WEBSITE
